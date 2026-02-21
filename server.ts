511bcb8162dd38eba22c3f0aba518ccb19fd1581
Chirani Siriwardhana <chiranisiriwardana@gmail.com>
2026-02-21 19:31:18 +0530

    Initial backend setup

---
+++ server.ts
@@ -0,0 +1,24 @@
+import "dotenv/config";
+import express, { Request, Response } from 'express';
+import cors from "cors";
+import connectDB from "./configs/db.js";
+
+await connectDB();
+const app = express();
+
+// Middleware
+app.use(cors())
+app.use(express.json());
+
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Server is Live!');
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
